e2d18d0380af0fa2f1e8eed0823ccbe3de90fd53
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2020-12-30 12:53:58 +0000

    tidied up code

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,51 +3,81 @@ using Test
 
 module wrapper1
 	using FromFile
+	visible = [:A]
+	invisible = [:foo, :bar, :baz, :quux, :B, :C]
+	
     @from "test/file.jl" import A
 end
 
 module wrapper2
 	using FromFile
+	visible = [:foo]
+	invisible = [:bar, :baz, :quux, :A, :B, :C]
+	
 	@from "test/file.jl" import A: foo
 end
 
 module wrapper3
 	using FromFile
+	visible = [:foo, :B]
+	invisible = [:bar, :baz, :quux, :A, :C]
+	
 	@from "test/file.jl" import A: foo, B
 end
 
 module wrapper4
 	using FromFile
+	visible = [:foo]
+	invisible = [:bar, :baz, :quux, :A, :B, :C]
+	
     @from "test/file.jl" import A.foo
 end
 
 module wrapper5
 	using FromFile
+	visible = [:foo, :B]
+	invisible = [:bar, :baz, :quux, :A, :B, :C]
+	
     @from "test/file.jl" import A.foo, A.B
 end
 
 module wrapper6
 	using FromFile
+	visible = [:A, :foo, :B]
+	invisible = [:bar, :baz, :quux, :C]
+	
 	@from "test/file.jl" using A
 end
 
 module wrapper7
 	using FromFile
+	visible = [:foo]
+	invisible = [:bar, :baz, :quux, :A, :B, :C]
+	
 	@from "test/file.jl" using A: foo
 end
 
 module wrapper8
 	using FromFile
+	visible = [:foo, :B]
+	invisible = [:bar, :baz, :quux, :A, :C]
+	
 	@from "test/file.jl" using A: foo, B
 end
 
 module wrapper9
 	using FromFile
+	visible = [:A, :C]
+	invisible = [:foo, :bar, :baz, :quux, :B]
+	
     @from "test/file.jl" import A, C
 end
 
 module wrapper10
 	using FromFile
+	visible = [:foo, :quux, :A, :B, :C]
+	invisible = [:bar, :baz]
+	
     @from "test/file.jl" using A, C
 end
 
@@ -62,52 +92,11 @@ end
 	@test !isdefined(@__MODULE__, :quux)
 	
 	for wrapper in (wrapper1, wrapper2, wrapper3, wrapper4, wrapper5, wrapper6, wrapper7, wrapper8, wrapper9, wrapper10)
-		@eval begin
-			# Make sure that the module A does or doesn't make it in
-			if $wrapper in (wrapper1, wrapper6, wrapper9, wrapper10)
-				@test isdefined($wrapper, :A)
-				@test isdefined($wrapper.A, :foo)
-				@test isdefined($wrapper.A, :bar)
-				@test $wrapper.A.foo() == "hello"
-				@test $wrapper.A.bar() == "goodbye"
-				@test isdefined($wrapper.A, :B)
-				@test isdefined($wrapper.A.B, :baz)
-				@test $wrapper.A.B.baz == 5
-			else
-				@test !isdefined($wrapper, :A)
-			end
-			
-			# Make sure that the various attributes all either do or don't make it in.
-			if $wrapper in (wrapper3, wrapper6, wrapper8, wrapper10)
-				@test isdefined($wrapper, :foo)
-				@test isdefined($wrapper, :B)
-			elseif $wrapper in (wrapper2, wrapper7)
-				@test isdefined($wrapper, :foo)
-				@test !isdefined($wrapper, :B)
-			elseif $wrapper === wrapper4
-				@test isdefined($wrapper.A, :foo)
-				@test !isdefined($wrapper.A, :B)
-			elseif $wrapper === wrapper5
-				@test isdefined($wrapper.A, :foo)
-				@test isdefined($wrapper.A, :B)
-			else
-				@test !isdefined($wrapper, :foo)
-				@test !isdefined($wrapper, :B)
-			end
-			@test !isdefined($wrapper, :bar)
-			
-			
-			# Make sure that the module C either does or doesn't make it in
-			if $wrapper === wrapper9
-				@test isdefined($wrapper, :C)
-				@test !isdefined($wrapper, :quux)
-			elseif $wrapper === wrapper10
-				@test isdefined($wrapper, :C)
-				@test isdefined($wrapper, :quux)
-			else
-				@test !isdefined($wrapper, :C)
-				@test !isdefined($wrapper, :quux)
-			end
+		for visible in wrapper.visible
+			@eval @test isdefined($wrapper, $(QuoteNode(visible)))
+		end
+		for invisible in wrapper.invisible
+			@eval @test !isdefined($wrapper, $(QuoteNode(invisible)))
 		end
 	end
 	
