f44a00d1253e2653a2d2c8824a4fb9ad50230fe7
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2021-01-01 00:24:05 +0000

    Fixes for paths; importing in packages due to closed modules

--- src/FromFile.jl
+++ src/FromFile.jl
@@ -2,33 +2,38 @@ module FromFile
 
 export @from
 
-# This replicates Base.__toplevel__
-baremodule __toplevel__
-    using Base
+macro from(path::String, ex::Expr)
+    esc(from_m(__module__, __source__, path, ex))
 end
 
-macro from(path::String, ex)
-    esc(from_m(__module__, path, ex))
-end
-
-function from_m(m::Module, path::String, ex::Expr)
+function from_m(m::Module, s::LineNumberNode, path::String, ex::Expr)
     ex.head === :using || ex.head === :import || error("expect using/import statement")
-    path = abspath(normpath(path))
-    loading = Expr(ex.head)
-    root = root_module(m)
-
-    if root === Main
-        toplevel = __toplevel__
+	
+	root = Base.moduleroot(m)
+	basepath = dirname(String(s.file))
+	
+    # file path should always be relative to the
+    # module loads it, unless specified as absolute
+    # path or the module is created interactively
+    if !isabspath(path) && basepath != ""
+        path = joinpath(basepath, path)
     else
-        toplevel_symbol = Symbol("#__toplevel__#")
-        if isdefined(root, toplevel_symbol) # package
-            toplevel = getfield(root, toplevel_symbol)
-        else
-            toplevel = Base.eval(root, :(baremodule $toplevel_symbol; using Base; end))
-        end
-    end
+		path = abspath(path)
+	end
+	
+    loading = Expr(ex.head)
     
-    file_module = load_module(toplevel, root, path)
+    if root === Main
+        file_module_sym = Symbol(path)
+    else
+        file_module_sym = Symbol(relpath(path, pathof(root)))
+    end
+
+    if isdefined(root, file_module_sym)
+        file_module = getfield(root, file_module_sym)
+    else
+        file_module = Base.eval(root, :(module $(file_module_sym); include($path); end))
+    end
 
     for each in ex.args
         each isa Expr || continue
@@ -46,33 +51,6 @@ function from_m(m::Module, path::String, ex::Expr)
         end
     end
     return loading
-end
-
-function load_module(toplevel::Module, root::Module, path::String)
-    if root === Main
-        file_module_sym = Symbol(path)
-    else
-        file_module_sym = Symbol(relpath(path, pathof(root)))
-    end
-
-    if isdefined(toplevel, file_module_sym)
-        file_module = getfield(toplevel, file_module_sym)
-    else
-        file_module = Base.eval(toplevel, :(module $(file_module_sym); end))
-        Base.include(file_module, path)
-    end
-
-    return file_module
-end
-
-function root_module(m::Module)
-    root = parentmodule(m)
-    prev = m
-    while root !== prev
-        prev = root
-        root = parentmodule(root)
-    end
-    return root
 end
 
 end
