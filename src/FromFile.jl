7a6a0404fc03636be5900208d79fff4ca610091d
Patrick Kidger <33688385+patrick-kidger@users.noreply.github.com>
2021-08-11 13:35:05 +0100

    Update FromFile.jl

--- src/FromFile.jl
+++ src/FromFile.jl
@@ -14,18 +14,17 @@ function from_m(m::Module, s::LineNumberNode, path::String, root_ex::Expr)
     end
     
     all(ex -> ex.head === :using || ex.head === :import, import_exs) || error("expected using/import statement")
-	
-	root = Base.moduleroot(m)
-	basepath = dirname(String(s.file))
-	
+
+    root = Base.moduleroot(m)
+    basepath = dirname(String(s.file))
+
     # file path should always be relative to the
     # module loads it, unless specified as absolute
     # path or the module is created interactively
     if !isabspath(path) && basepath != ""
         path = joinpath(basepath, path)
-    else
-        path = abspath(path)
     end
+    path = abspath(path)
 	
     
     if root === Main
