13e13d65e2796d5eda251d7c79811d8713d81766
Daniel Soutar <daniel.soutar@codeplay.com>
2022-05-06 05:10:36 +0100

    Add transform to handle aliasing edge-cases (#40)
    * Support `import X as Y` case
    
    * Generalise first case to handle aliasing

--- src/FromFile.jl
+++ src/FromFile.jl
@@ -61,9 +61,9 @@ function from_m(m::Module, s::LineNumberNode, path::String, root_ex::Expr)
         for each in ex.args
             each isa Expr || continue
 
-            if each.head === :(:) # using/import A: a, b, c
+            if each.head === :(:) || each.head === :as # using/import A: a, b, c or import A as B
                 each.args[1].args[1] === :(.) && error("cannot load relative module from file")
-                push!(loading.args, Expr(:(:), Expr(:., fullname(file_module)..., each.args[1].args...), each.args[2:end]...) )
+                push!(loading.args, Expr(each.head, Expr(:., fullname(file_module)..., each.args[1].args...), each.args[2:end]...) )
             elseif each.head === :(.) # using/import A, B.C
                 each.args[1] === :(.) && error("cannot load relative module from file")
                 push!(loading.args, Expr(:., fullname(file_module)..., each.args...))
